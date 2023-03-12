5790cc5d197bd2d82cccc2ade8fb4c03830110b5
tahsinsy <tahsinsy@gmail.com>
2023-03-12 00:50:50 -0500

    js logic added

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -1,9 +1,64 @@
 
-var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+// var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
-for (var x = 0; x < topics.length; x++) {
- console.log(topics[x]);
+// for (var x = 0; x < topics.length; x++) {
+//  console.log(topics[x]);
+// }
+
+var list = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var randomTopic = list[Math.floor(Math.random()*list.length)];
+
+
+function listTopics() {
+
+    for (var x=0; x < list.length; x++) {
+
+        console.log(list[x]);
+
+    }
+
+    //createing random selction of topic
+
 }
+
+function selectTopic() {
+
+    if (randomTopic === 'HTML') {
+
+        console.log("let's study HTML");
+    }
+
+    else if (randomTopic === 'CSS') {
+        console.log("let's study CSS");
+
+    }
+
+    else if (randomTopic === 'git') {
+
+        console.log("Let's study git");
+
+    }
+
+    else if (randomTopic === 'JavaScript') {
+        console.log("Let's study JS");
+
+
+    }
+
+    else {
+
+        console.log("nada !!");
+    }
+
+
+}
+
+console.log("Here are the topics we learned!!")
+listTopics();
+
+console.log("Which topic should we study first?")
+selectTopic();
+
 
 /*
 if (topic === 'HTML') {
